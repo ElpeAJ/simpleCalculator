70a41f9a4f4660d363913f4a4a165ec91c5e55b9
Elpedia Junior Arthur <elpediaarthurjunior@gmail.com>
2024-01-23 14:46:24 -0800

    Made some changes to the main file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//select all the operators 
+//select all the operators
 let plus = document.querySelector("#plus");
 let minus = document.querySelector("#minus");
 let divide = document.querySelector("#divide");
@@ -17,19 +17,18 @@ let nine = document.querySelector("#nine");
 let zero = document.querySelector("#zero");
 
 //select all the symbols
-let result = document.querySelector("#result");
+let equal = document.querySelector("#equal");
 let dot = document.querySelector("#dot");
 let clear = document.querySelector("#clear");
 
+//function for all the numbers
 function numOne() {
   result.innerHTML += "1"; // Option 1: Append using +=
-
   return 1;
 }
 
 function numTwo() {
-  result.innerHTML += "2"; // Option 1: Append using +=
-
+  result.innerHTML += "2";
   return 2;
 }
 
@@ -39,24 +38,22 @@ function numThree() {
 }
 
 function numFour() {
-  result.insertAdjacentHTML("beforeend", "4"); // Option 2: Use insertAdjacentHTML
+  result.insertAdjacentHTML("beforeend", "4");
   return 4;
 }
 
 function numFive() {
-  result.innerHTML += "5"; // Option 1: Append using +=
-
+  result.innerHTML += "5";
   return 5;
 }
 
 function numSix() {
-  result.innerHTML += "6"; // Option 1: Append using +=
-
+  result.innerHTML += "6";
   return 6;
 }
 
 function numSeven() {
-  result.insertAdjacentHTML("beforeend", "7"); // Option 2: Use insertAdjacentHTML
+  result.insertAdjacentHTML("beforeend", "7");
   return 7;
 }
 
@@ -65,28 +62,56 @@ function numEight() {
   return 8;
 }
 
-function numOne() {
-  result.innerHTML += "1"; // Option 1: Append using +=
-
-  return 1;
+function numNine() {
+  result.innerHTML += "9";
+  return 9;
 }
 
-function numTwo() {
-  result.innerHTML += "2"; // Option 1: Append using +=
-
-  return 2;
+function numZero() {
+  result.innerHTML += "0"; // Option 1: Append using +=
+  return 0;
 }
 
-function numThree() {
-  result.insertAdjacentHTML("beforeend", "3"); // Option 2: Use insertAdjacentHTML
-  return 3;
+//function for symbols
+function symDot() {
+  result.insertAdjacentHTML("beforeend", ".");
+  return `.`;
 }
 
-function numFour() {
-  result.insertAdjacentHTML("beforeend", "4"); // Option 2: Use insertAdjacentHTML
-  return 4;
+function symClear() {
+  result.innerHTML = " ";
+  clear.innerHTML = "AC";
+}
+function symEqual() {
+  // result.insertAdjacentHTML("beforeend", "=");
+  // return `=`;
+  let finalResult = result.innerHTML;
+  finalResult = eval(finalResult);
+  result.innerHTML = finalResult;
 }
 
+//functions for operators
+function plusSign() {
+  result.insertAdjacentHTML("beforeend", "+"); // Option 2: Use insertAdjacentHTML
+  return `+`;
+}
+
+function minusSign() {
+  result.insertAdjacentHTML("beforeend", "-");
+  return `-`;
+}
+
+function divideSign() {
+  result.insertAdjacentHTML("beforeend", "/");
+  return `/`;
+}
+
+function multiplySign() {
+  result.insertAdjacentHTML("beforeend", "*");
+  return `*`;
+}
+
+//Event listener for numbers
 one.addEventListener("click", numOne);
 two.addEventListener("click", numTwo);
 three.addEventListener("click", numThree);
@@ -97,3 +122,32 @@ seven.addEventListener("click", numSeven);
 eight.addEventListener("click", numEight);
 nine.addEventListener("click", numNine);
 zero.addEventListener("click", numZero);
+
+//Event listeners for symbols
+equal.addEventListener("click", symEqual);
+dot.addEventListener("click", symDot);
+clear.addEventListener("click", symClear);
+
+//Event listeners for operators
+plus.addEventListener("click", plusSign);
+minus.addEventListener("click", minusSign);
+divide.addEventListener("click", divideSign);
+multiply.addEventListener("click", multiplySign);
+
+//Create objects of all the return for the functions
+// objOne = numOne();
+// objTwo = numTwo();
+// objThree = numThree();
+// objFour = numFour();
+// objFive = numFive();
+// objSix = numSix();
+// objSeven = numSeven();
+// objEight = numEight();
+// objNine = numNine();
+// objZero = numZero();
+// objPlus = plusSign();
+// objMinus = minusSign();
+// objDivide = divideSign();
+// objMultiply = multiplySign();
+// objDot = symDot();
+// objEqual = symEqual();
